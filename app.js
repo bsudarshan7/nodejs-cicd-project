f2be843461d7cc0bf89bd4b75c943d0fbbf36b67
sudarshan7 <birajdarsudarshan6@gmail.com>
2026-06-21 12:02:02 +0000

    Fix app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express123 = require("express");
+const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
@@ -27,9 +27,7 @@ res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/health", (req, res) => {
-res.json({
-status: "healthy"
-});
+res.json({ status: "healthy" });
 });
 
 app.get("/tasks", (req, res) => {
@@ -37,40 +35,27 @@ res.json(readTasks());
 });
 
 app.post("/tasks", (req, res) => {
-
-```
 const tasks = readTasks();
 
 const task = {
-    id: Date.now(),
-    task: req.body.task
+id: Date.now(),
+task: req.body.task
 };
 
 tasks.push(task);
-
 saveTasks(tasks);
 
 res.json(task);
-```
-
 });
 
 app.delete("/tasks/:id", (req, res) => {
-
-```
 let tasks = readTasks();
 
-tasks = tasks.filter(
-    t => t.id != req.params.id
-);
+tasks = tasks.filter(t => t.id != req.params.id);
 
 saveTasks(tasks);
 
-res.json({
-    message: "Task Deleted"
-});
-```
-
+res.json({ message: "Task Deleted" });
 });
 
 app.listen(PORT, "0.0.0.0", () => {
